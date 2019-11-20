6003fe2156367c84ce9b4fb22c90b6f6bc1711ec
Miles Higbie <miles.higbie@gmail.com>
2019-11-20 01:11:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,10 +2,8 @@ def base_hash
 	{railroads: {}}
 end
 
-def monopoly_with_second_tier{
-  	railroads: {:pieces => 4 }
-  	  
-  	}
+def monopoly_with_second_tier
+  {railroads: {}}
 end
 
 def monopoly_with_third_tier
